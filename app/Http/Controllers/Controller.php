773962fd507093532877c83fa2f0c878ef928f9c
IlliaHorishnii <gorishnij03@gmail.com>
2021-05-06 19:36:19 +0300

    formation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,10 +9,10 @@ use Illuminate\Routing\Controller as BaseController;
 /**
  * @OA\Info (
  *     version="1.0.0",
- *     title="TEST API",
- *     description="Description of API",
+ *     title="Orders API",
+ *     description="API requests",
  *     @OA\Contact (
- *     email="test@gmail.com"
+ *     email="company@gmail.com"
  *      ),
  *     @OA\License (
  *     name="Apache 2.0",
